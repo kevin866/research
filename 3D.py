3345441be00e3ae2458a773274454c6df8e928df
kevin886 <k.wang1502483@gmail.com>
2023-05-09 17:29:34 -0700

    commit

--- 3D.py
+++ 3D.py
@@ -12,23 +12,25 @@ from random import randint
 curve = BSpline.Volume()
 
 # Set degree
-curve.degree_u = 1
-curve.degree_v = 1
-curve.degree_w = 1
-curve.ctrlpts_size_u=8
-curve.ctrlpts_size_v=8
-curve.ctrlpts_size_w=8
+curve.degree_u = 2
+curve.degree_v = 2
+curve.degree_w = 2
+curve.ctrlpts_size_u=3
+curve.ctrlpts_size_v=3
+curve.ctrlpts_size_w=3
 # Set control points
 #
-curve.ctrlpts = [[float(randint(0, 100)) for _ in range(3)] for _ in range(512)]
+curve.ctrlpts = tools.random_digits(3,6,6)
+#tools.random_digits(3,6,6)
 #
+#curve.ctrlpts = [[float(randint(0, 10)) for _ in range(3)] for _ in range(512)]
 
-print(curve.ctrlpts_size)
+#print(curve.ctrlpts)
 # Set knot vector
-curve.knotvector_u = utilities.generate_knot_vector(curve.degree_u, 8)
-curve.knotvector_v = utilities.generate_knot_vector(curve.degree_v, 8)
-curve.knotvector_w = utilities.generate_knot_vector(curve.degree_w, 8)
-
+curve.knotvector_u = utilities.generate_knot_vector(curve.degree_u, 3)
+curve.knotvector_v = utilities.generate_knot_vector(curve.degree_v, 3)
+curve.knotvector_w = utilities.generate_knot_vector(curve.degree_w, 3)
+#print(curve.knotvector_w)
 # Set evaluation delta (controls the number of curve points)
 curve.delta_u = 0.05
 curve.delta_v = 0.05
@@ -38,4 +40,4 @@ from matplotlib import cm
 
 # Plot the control points grid and the evaluated surface
 curve.vis = VisVTK.VisSurface()
-curve.render(colormap=cm.cool)
+curve.render(colormap=cm.cool, animate=True)
--- dummy.py
+++ dummy.py
@@ -1,3 +1,5 @@
 import tools
-print(tools.random_digits(8,8,8))
+#print(tools.random_digits(8,8,8))
 #tools.generate_surface(tools.random_digits(3,6,6), 2,2)
+import geomdl.shapes
+geomdl.shapes
